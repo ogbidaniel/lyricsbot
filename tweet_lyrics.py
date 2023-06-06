a104ce23e9bd3c63da2572b5b331683dc012be2b
Daniel Ogbuigwe <103327095+Ekwesu@users.noreply.github.com>
2023-06-06 22:06:49 +0100

    Update tweet_lyrics.py
    Removed Api Keys from open visibility

--- tweet_lyrics.py
+++ tweet_lyrics.py
@@ -1,15 +1,15 @@
 import tweepy
 import lyricspicker
 
-consumer_key = "UD5mN3IfRWTv9hPSwkoJsZT99"
-consumer_secret = "A1hc72jVdLQUQFYi5sh8DJyV2n2IdfyXgWx2LZO4RoXFrmqTTk"
-access_token = "1330582472418070530-bMuvf6MIIgibwtRDahjnhMwctmisv8"
-access_token_secret = "rBTuD4YjljWGT7E601rbeQRDkyYt5Y6Lf5kwreunGHndp"
-bearer_token = "AAAAAAAAAAAAAAAAAAAAACVWnwEAAAAAhVbY%2F%2B%2FsjR%2BlbD4SKJxpm50cazA%3D3fvJIX7xOwVktRyw3fnKWldLDPJJ9D7mJ0RnPdWyEoy3zFYyni"
+consumer_key = "ENTER KEY"
+consumer_secret = "ENTER KEY"
+access_token = "ENTER KEY"
+access_token_secret = "ENTER KEY"
+bearer_token = "ENTER KEY"
 
 # Create a Client instance and use it to post a tweet
 client = tweepy.Client(
-    bearer_token="AAAAAAAAAAAAAAAAAAAAACVWnwEAAAAAhVbY%2F%2B%2FsjR%2BlbD4SKJxpm50cazA%3D3fvJIX7xOwVktRyw3fnKWldLDPJJ9D7mJ0RnPdWyEoy3zFYyni",
+    bearer_token="ENTER KEY",
     access_token=access_token,
     access_token_secret=access_token_secret,
     consumer_key=consumer_key,
