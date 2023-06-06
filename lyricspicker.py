4ffedbb4fcec777fc4d262e2ca05178de0fe8c69
Ekwesu <103327095+Ekwesu@users.noreply.github.com>
2023-06-06 12:01:21 +0100

    Update lyricspicker.py

--- lyricspicker.py
+++ lyricspicker.py
@@ -2,7 +2,6 @@
 import random
 import os
 
-# Define a function called get_line that returns a random line from the lyrics
 def get_line():
     # Specify the file path of the albums folder
     albums_path = "C:\\Users\\maver\\Documents\\Python\\Projects\\tweepy-bots\\albums"
@@ -10,10 +9,8 @@ def get_line():
     # Get the list of album folders in the albums folder
     albums = [folder for folder in os.listdir(albums_path) if os.path.isdir(os.path.join(albums_path, folder))]
 
-    # Choose a random album from the list
     random_album = random.choice(albums)
 
-    # Get the list of song files in the random album folder
     songs = [file for file in os.listdir(os.path.join(albums_path, random_album)) if file.endswith(".txt")]
 
     # Choose a random song from the list
@@ -24,14 +21,13 @@ def get_line():
         # Read the file and split it into a list of lines
         lines = f.read().split("\n")
 
-    # Ignore the first line of the lyrics by slicing the list from the second element
+    # Ignore the first line of the lyrics
     lines = lines[1:]
 
-    # Filter out any line that has "\" or "Embed" using list comprehension
+    # Filters out any line that has "\" or "Embed"
     lines = [line for line in lines if "\\" not in line and "Embed" not in line]
 
     # Choose a random line from the list
     random_line = random.choice(lines)
 
-    # Return the random line
     return random_line
